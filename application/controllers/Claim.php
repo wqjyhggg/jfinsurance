71a5b824d90dc3c6becf8210227f302a19315328
Jack1232 <wqjyhggg@gmail.com>
2016-06-15 15:29:51 -0400

    build

---
+++ application/controllers/Claim.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Claim extends MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		$data['title_txt'] = 'Welcome';
+		$data['top_menu'] = $this->menu_model->load_top_meun();
+		$data['menu'] = $this->menu_model->load_meun();
+		$this->load->common('home', $data);
+	}
+}
