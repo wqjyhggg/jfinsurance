5eea480c86143b756da5d70af18cacb39a91b24a
zoewen <zoeywenyue@gmail.com>
2016-06-20 17:49:35 -0400

    improved layout

--- application/views/user/dtlist.php
+++ application/views/user/dtlist.php
@@ -69,63 +69,128 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <br />
 
 
-
 					<?php if (!empty($error_message)) { echo $error_message . "<br>"; } ?>
-					<form action='<?php $action_url; ?>' method='POST'>
-					<input type='hidden' name='<?php echo $csrf['name']; ?>' value='<?php echo $csrf['value']; ?>'><br>
-					<select name='user_group_id'>
-					<option value='0'> -- select user group -- </option>
-					<?php foreach ($user_group_list as $key => $name) { ?>
-					<option value='<?php echo $key; ?>'><?php echo $name; ?></option>
-					<?php } ?>
-					</select><br>
-					Username: <input type='text' name='username' value='<?php echo $username; ?>'><br>
-					Firstname: <input type='text' name='firstname' value='<?php echo $firstname; ?>'><br>
-					Lastname: <input type='text' name='lastname' value='<?php echo $lastname; ?>'><br>
-					Email: <input type='text' name='email' value='<?php echo $email; ?>'><br>
-					Brockerage: <input type='text' name='business' value='<?php echo $business; ?>'><br>
-					<input type='submit'><br>
-					</form> 
-
-					<p>User List <a href='<?php echo $edit_url."0"; ?>'>Add user</a></p>
-					<table>
-						<tr>
-							<th>Username</th>
-							<th>Type</th>
-							<th>Brokerage</th>
-							<th>Name</th>
-							<th>Email</th>
-							<th>Licence#</th>
-							<th>Expire</th>
-							<th>Pay Types</th>
-							<th>Status</th>
-						</tr>
-					<?php foreach ($user_list as $user) { ?>
-						<tr>
-							<td><a href='<?php echo $edit_url.$user['user_id']; ?>'><?php echo $user['username']?></a></td>
-							<td><?php echo $user_group_list[$user['user_group_id']]; ?></td>
-							<td><?php echo $user['business']; ?></td>
-							<td><?php echo $user['lastname'] . " " . $user['lastname']; ?></td>
-							<td><?php echo $user['email']; ?></td>
-							<td><?php echo $user['licence_number']; ?></td>
-							<td><?php echo $user['licence_expire']; ?></td>
-							<td><?php echo $user['pay_type']; ?></td>
-							<td><?php echo $user['status'] ? 'Act' : '-'; ?></td>
-						</tr>
-					<?php } ?>
-					</table>
-
-
-
+					<form action='<?php $action_url; ?>' method='POST' class="form-horizontal">
+					  <input type='hidden' name='<?php echo $csrf['name']; ?>' value='<?php echo $csrf['value']; ?>'><br>
+  					
+            <div class="row">
+              <div class="col-sm-3 form-group">
+                  <label class="col-sm-12">User Type:</label>
+                  <div class="col-sm-12 input-group">
+                    <select name='user_group_id' class="form-control">
+                      <option value='0'> -- select user group -- </option>
+                      <?php foreach ($user_group_list as $key => $name) { ?>
+                      <option value='<?php echo $key; ?>'><?php echo $name; ?></option>
+                      <?php } ?>
+                    </select>
+                  </div>
+              </div>
+              <div class="form-group col-sm-3">
+                <label class="col-sm-12">Brocker Age:</label>
+                <div class="input-group col-sm-12">
+                  <input type='text' name='business' value='<?php echo $business; ?>' class="form-control">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <!-- Last Name input box -->
+              <div class="form-group col-sm-3">
+                <label class="col-sm-12">Last Name:</label>
+                <div class="input-group col-sm-12">
+                  <input type='text' name='lastname' value='<?php echo $lastname; ?>' class="form-control">
+                </div>
+              </div>
+              <!-- Last Name input box end -->
+              <!-- First Name input box -->
+              <div class="form-group col-sm-3">
+                <label class="col-sm-12">First Name:</label>
+                <div class="input-group col-sm-12">
+                   <input type='text' name='firstname' value='<?php echo $firstname; ?>' class="form-control">
+                 </div>
+              </div>
+          
+              <!-- User Name input box -->
+              <div class="form-group col-sm-3">
+                <label class="col-sm-12">Username:</label>
+                <div class="input-group col-sm-12">
+                  <input type='text' name='username' value='<?php echo $username; ?>' class="form-control">
+                 </div>
+              </div>
+              <!-- Email input box -->
+              <div class="form-group col-sm-3">
+                <label class="col-sm-12">E-amil:</label>
+                <div class="input-group col-sm-12">
+                  <input type='text' name='email' value='<?php echo $email; ?>' class="form-control">
+                </div>
+              </div>
+            </div>
+				
+       
+			      <div class="row">
+              <!-- submit button -->
+              <div class="col-sm-12">
+                <button class="btn btn-default pull-right">Start Search</button>
+              </div> 
+              <!-- submit button -->
+            </div>
+					  
+					</form>
                     
                   </div>
                 </div>
               </div>
             </div>
 
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>User List<small><a href='<?php echo $edit_url."0"; ?>'>Add user</a></small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    
+                    <div class="table-responsive">
+                      <table class="table table-hover table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Username</th>
+                            <th>Type</th>
+                            <th>Brokerage</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Licence#</th>
+                            <th>Expire</th>
+                            <th>Pay Types</th>
+                            <th>Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($user_list as $user) { ?>
+                          <tr>
+                            <td><a href='<?php echo $edit_url.$user['user_id']; ?>'><?php echo $user['username']?></a></td>
+                            <td><?php echo $user_group_list[$user['user_group_id']]; ?></td>
+                            <td><?php echo $user['business']; ?></td>
+                            <td><?php echo $user['lastname'] . " " . $user['lastname']; ?></td>
+                            <td><?php echo $user['email']; ?></td>
+                            <td><?php echo $user['licence_number']; ?></td>
+                            <td><?php echo $user['licence_expire']; ?></td>
+                            <td><?php echo $user['pay_type']; ?></td>
+                            <td><?php echo $user['status'] ? 'Act' : '-'; ?></td>
+                          </tr>
+                        <?php } ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+
         </div>
-        <!-- /page content -->
+      </div>
+      <!-- /page content -->
         
 
 
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -1,77 +1,324 @@
-<p>Edit Form</p>
-<?php if (!empty($error_message)) { echo $error_message . "<br>"; } ?>
-<form action='<?php $action_url; ?>' method='POST'>
-<input type='hidden' name='<?php echo $csrf['name']; ?>' value='<?php echo $csrf['value']; ?>'>
-<input type='hidden' name='user_id' value='<?php echo $user_id; ?>'>
-<select name='user_group_id'>
-<option value='0' <?php echo empty($user_group_id) ? 'selected' : ''; ?>> -- select user group -- </option>
-<?php foreach ($user_group_list as $key => $name) { ?>
-<option value='<?php echo $key; ?>' <?php echo ($user_group_id == $key) ? 'selected' : ''; ?>><?php echo $name; ?></option>
-<?php } ?>
-</select><br>
-<select name='parent_user_id'>
-<option value='0' <?php echo empty($parent_user_id) ? 'selected' : ''; ?>> -- select brokerage -- </option>
-<?php foreach ($broker_list as $key => $name) { ?>
-<option value='<?php echo $key; ?>' <?php echo ($parent_user_id == $key) ? 'selected' : ''; ?>><?php echo $name; ?></option>
-<?php } ?>
-</select><br>
-Username: <input type='text' name='username' value='<?php echo $username; ?>'><br>
-Password: <input type='text' name='password' value=''><br>
-Region: <input type='text' name='region' value='<?php echo $region; ?>'><br>
-Brokerage: <input type='text' name='business' value='<?php echo $business; ?>'><br>
-Gender: 
-<select name='gender'>
-<option value='M' <?php echo (empty($gender) || ($gender == 'M')) ? 'selected' : ''; ?>> M - Male </option>
-<option value='F' <?php echo ($gender == 'F') ? 'selected' : ''; ?>> F - Female </option>
-</select><br>
-Firstname: <input type='text' name='firstname' value='<?php echo $firstname; ?>'><br>
-Lastname: <input type='text' name='lastname' value='<?php echo $lastname; ?>'><br>
-Email: <input type='text' name='email' value='<?php echo $email; ?>'><br>
-Address: <input type='text' name='address' value='<?php echo $address; ?>'><br>
-City: <input type='text' name='city' value='<?php echo $city; ?>'><br>
-Province: <div id='province2_div'></div><br>
-Country: <div id='country2_div'></div><br>
-Post Code: <input type='text' name='postcode' value='<?php echo $postcode; ?>'><br>
-Website: <input type='text' name='website' value='<?php echo $website; ?>'><br>
-Licence#: <input type='text' name='licence_number' value='<?php echo $licence_number; ?>'><br>
-Licence Expire: <input type='date' name='licence_expire' value='<?php echo $licence_expire; ?>'><br>
-Business Phone: <input type='text' name='business_phone' value='<?php echo $business_phone; ?>'><br>
-Mobile Phone: <input type='text' name='mobile_phone' value='<?php echo $mobile_phone; ?>'><br>
-FAX: <input type='text' name='fax_number' value='<?php echo $fax_number; ?>'><br>
-Toll Free: <input type='text' name='toll_free' value='<?php echo $toll_free; ?>'><br>
-<b>Products:</b><br>
-<?php foreach ($product_list as $key => $pd) { ?>
-<input type='checkbox' name='product_list[]' value='<?php echo $key; ?>' <?php echo $pd['checked']; ?>> <?php echo $pd['product_short'] . " - " . $pd['full_name']; ?><br>
-<?php echo $pd['product_short']; ?> Commission (%): <input type='text' name='product_commission_<?php echo $key; ?>' value='<?php echo $pd['commission']; ?>'><br>
-<?php } ?>
-<b>Pay type:</b><br>
-<?php foreach ($paytype_list as $pay) { ?>
-<input type='checkbox' name='paytype_list[]' value='<?php echo $pay; ?>' <?php echo (strpos($pay_type, $pay) === FALSE) ? '' : 'checked'; ?>> <?php echo $pay; ?><br>
-<?php } ?>
-<b>Status:</b><br>
-<select name='status'>
-<option value='1' <?php echo ($status || empty($user_id)) ? 'selected' : ''; ?>>Active</option>
-<option value='0' <?php echo (empty($status) && $user_id) ? 'selected' : ''; ?>>Disable</option>
-</select><br>
-<b>Notes:</b><br>
-<textarea name='note'><?php echo $note; ?></textarea><br>
-<input type='submit' value='<?php echo ($user_id) ? "Update" : "Add"; ?>'><br>
-</form>
-<script>
-$( document ).ready(function() {
-	$.ajax({
-		url: '<?php echo $province_url; ?>',
-		type: 'GET',
-		success: function(data, textStatus, jqXHR) {
-        	$('#province2_div').html(data);
-    	},
-	});
-	$.ajax({
-		url: '<?php echo $country_url; ?>',
-		type: 'GET',
-		success: function(data, textStatus, jqXHR) {
-        	$('#country2_div').html(data);
-    	},
-	});
-});
-</script>
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!-- Report/ Sales report to JF page content -->
+
+	    <!-- Content top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav class="" role="navigation">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <!-- User section -->
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">John Doe
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Profile</a></li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                    </li>
+                    <li><a href="javascript:;">Help</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                  </ul>
+                </li>
+                <!-- User section End -->
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- Content top navigation End-->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Add New User</h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Edit Form<small></small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+
+					<?php if (!empty($error_message)) { echo $error_message . "<br>"; } ?>
+					<form action='<?php $action_url; ?>' method='POST' class="form-horizontal">
+
+					<input type='hidden' name='<?php echo $csrf['name']; ?>' value='<?php echo $csrf['value']; ?>'>
+					<input type='hidden' name='user_id' value='<?php echo $user_id; ?>'>
+					
+					<div class="row">
+						<div class="col-sm-3 form-group">
+		                  <label class="col-sm-12">User Type:</label>
+		                  <div class="col-sm-12 input-group">
+							<select name='user_group_id' class="form-control">
+							<option value='0' <?php echo empty($user_group_id) ? 'selected' : ''; ?>> -- select user group -- </option>
+							<?php foreach ($user_group_list as $key => $name) { ?>
+							<option value='<?php echo $key; ?>' <?php echo ($user_group_id == $key) ? 'selected' : ''; ?>><?php echo $name; ?></option>
+							<?php } ?>
+							</select>
+						  </div>
+	              		</div>
+	              		<div class="col-sm-3 form-group">
+		                  <label class="col-sm-12">User Type:</label>
+		                  <div class="col-sm-12 input-group">
+		                  	<select name='parent_user_id' class="form-control">
+								<option value='0' <?php echo empty($parent_user_id) ? 'selected' : ''; ?>> -- select brokerage -- </option>
+								<?php foreach ($broker_list as $key => $name) { ?>
+								<option value='<?php echo $key; ?>' <?php echo ($parent_user_id == $key) ? 'selected' : ''; ?>><?php echo $name; ?></option>
+								<?php } ?>
+							</select>
+		                  </div>
+	              		</div>
+	              		<div class="form-group col-sm-3">
+			                <label class="col-sm-12">Region:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='region' value='<?php echo $region; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Brokerage:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='business' value='<?php echo $business; ?>' class="form-control">
+			                </div>
+			            </div>
+
+	              	</div>
+	              	<div class="row">
+	              		<div class="form-group col-sm-3">
+			                <label class="col-sm-12">Username:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='username' value='<?php echo $username; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Brokerage:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='password' value='' class="form-control" class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-2">
+			                <label class="col-sm-12">First Name:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='firstname' value='<?php echo $firstname; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-2">
+			                <label class="col-sm-12">Last Name:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='lastname' value='<?php echo $lastname; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-2">
+			                <label class="col-sm-12">Gender:</label>
+			                <div class="col-sm-12 input-group">
+				                <select name='gender' class="form-control">
+									<option value='M' <?php echo (empty($gender) || ($gender == 'M')) ? 'selected' : ''; ?>> M - Male </option>
+									<option value='F' <?php echo ($gender == 'F') ? 'selected' : ''; ?>> F - Female </option>
+								</select>
+							</div>
+			            </div>
+	              	</div>
+
+	              	<div class="row">
+	              		<div class="form-group col-sm-3">
+			                <label class="col-sm-12">E-mail:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='email' value='<?php echo $email; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-9">
+			                <label class="col-sm-12">Address:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='address' value='<?php echo $address; ?>' class="form-control">
+			                </div>
+			            </div>
+			        </div>
+			        <div class="row">
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">City:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='city' value='<?php echo $city; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Province:</label>
+			                <div class="input-group col-sm-12">
+			                	<div id='province2_div'></div>
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Country:</label>
+			                <div class="input-group col-sm-12">
+			                	<div id='country2_div'></div>
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Post Code:</label>
+			                <div class="col-sm-12 input-group">
+				                <input type='text' name='postcode' value='<?php echo $postcode; ?>' class="form-control">
+							</div>
+			            </div>
+	              	</div>
+	              	<div class="row">
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Website:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='website' value='<?php echo $website; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Licence No.:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='licence_number' value='<?php echo $licence_number; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Licence Expire Date:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='date' name='licence_expire' value='<?php echo $licence_expire; ?>' class="form-control">
+			                </div>
+			            </div>
+			            
+	              	</div>
+					<div class="row">
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Business Phone:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='business_phone' value='<?php echo $business_phone; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Mobile Phone:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='mobile_phone' value='<?php echo $mobile_phone; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Fax:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='fax_number' value='<?php echo $fax_number; ?>' class="form-control">
+			                </div>
+			            </div>
+			            <div class="form-group col-sm-3">
+			                <label class="col-sm-12">Toll Free:</label>
+			                <div class="input-group col-sm-12">
+			                	<input type='text' name='toll_free' value='<?php echo $toll_free; ?>' class="form-control">
+			                </div>
+			            </div>
+			            
+	              	</div>
+					<hr />
+					<div class="row">
+						<div class="form-group col-sm-12">
+							<h4>Products:</h4>
+
+							<?php foreach ($product_list as $key => $pd) { ?>
+							<div class="col-sm-6">
+							<input  type='checkbox' name='product_list[]' value='<?php echo $key; ?>' <?php echo $pd['checked']; ?>> <?php echo $pd['product_short'] . " - " . $pd['full_name']; ?><br>
+							<?php echo $pd['product_short']; ?> Commission (%): <input type='text' name='product_commission_<?php echo $key; ?>' value='<?php echo $pd['commission']; ?>'><br>
+							<br />
+							</div>
+							<?php } ?>
+						</div>
+
+					</div>
+					
+				
+					<div class="row">
+						<div class="form-group col-sm-6">
+							<p><b>Pay type:</b></p>
+							<?php foreach ($paytype_list as $pay) { ?>
+							<div class="col-sm-4">
+							<input type='checkbox' name='paytype_list[]' value='<?php echo $pay; ?>' <?php echo (strpos($pay_type, $pay) === FALSE) ? '' : 'checked'; ?>> <?php echo $pay; ?><br>
+							</div>
+							<?php } ?>
+						</div>
+						<div class="form-group col-sm-6">
+							<label class="col-sm-12">Status:</label>
+			                <div class="col-sm-12 input-group">
+								<select name='status' class="form-control">
+									<option value='1' <?php echo ($status || empty($user_id)) ? 'selected' : ''; ?>>Active</option>
+									<option value='0' <?php echo (empty($status) && $user_id) ? 'selected' : ''; ?>>Disable</option>
+								</select>
+							</div>
+					    </div>
+
+					</div>
+
+					
+					<div class="row">
+						<label class="col-sm-12">Notes:</label>
+			            <div class="col-sm-12 input-group">
+			            	<textarea name='note' class="form-control"><?php echo $note; ?></textarea>
+			            </div>
+					</div>
+
+					<div class="row">
+		              <!-- submit button -->
+		              <div class="col-sm-12">
+		              	<input class="btn btn-default pull-right" type='submit' value='<?php echo ($user_id) ? "Update" : "Add"; ?>'>
+		              </div> 
+		              <!-- submit button -->
+		            </div>
+					
+					<br>
+					
+					</form>
+
+					<script>
+					$( document ).ready(function() {
+						$.ajax({
+							url: '<?php echo $province_url; ?>',
+							type: 'GET',
+							success: function(data, textStatus, jqXHR) {
+					        	$('#province2_div').html(data);
+					    	},
+						});
+						$.ajax({
+							url: '<?php echo $country_url; ?>',
+							type: 'GET',
+							success: function(data, textStatus, jqXHR) {
+					        	$('#country2_div').html(data);
+					    	},
+						});
+					});
+					</script>
+
+
+ 				  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <!-- /page content -->
