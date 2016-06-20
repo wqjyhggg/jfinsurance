3a8fef16f9a20e74d1f3a21a9b54242dadfb7f60
zoewen <zoeywenyue@gmail.com>
2016-06-20 14:29:25 -0400

    improve page detail-report part

--- application/views/reports/agent.php
+++ application/views/reports/agent.php
@@ -58,6 +58,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               </div>
             </div>
             <div class="clearfix"></div>
+            <!-- Filter Section -->
             <div class="row">
               <!-- Common Filter Form -->
               <div class="col-md-12 col-sm-12 col-xs-12">
@@ -371,8 +372,125 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </div>
               </div>
               <!-- Duration Report Filter Form End-->
-
-            </div>
+            </div><!-- Filter Section End -->
+            <!-- List Section -->
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Search Result<small></small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <div class="table-responsive">
+                      <table class="table table-hover table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Travel Plan Number</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Gender</th>
+                            <th>Birthdate</th>
+                            <th>Address Line 1</th>
+                            <th>City</th>
+                            <th>Province</th>
+                            <th>Postal Code</th>
+                            <th>Option</th>
+                            <th>Agent</th>
+                            <th>Application Date</th>
+                            <th>Create Date</th>
+                            <th>Effective Date</th>
+                            <th>Expiry Date</th>
+                            <th>Number of Days</th>
+                            <th>Sum Insured</th>
+                            <th>Net Premium</th>
+                           
+                            <th>Gross Premium</th>
+                           
+                            <th>Rate Per Day</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                              <td colspan="22">JF Optimum Plus</td>
+                            </tr>
+                            <tr>
+                              <td>OPL248778</td>
+                              <td>TestGiven</td>
+                              <td>TestLast</td>
+                              <td>M</td>
+                              <td>1980-05-03</td>
+                              <td>14 Asdf fdsfsdf</td>
+                              <td>Toronto</td>
+                              <td>ON</td>
+                              <td>M2M 4M5</td>
+                              <td>9283</td>
+                              <td>458286</td>
+                              <td>2016-06-05</td>
+                              <td>2016-05-05</td>
+                              <td>2016-08-03</td>
+                              <td>2017-08-02</td>
+                              <td>365</td>
+                              <td>10,000</td>
+                              <td>733.21</td>
+                              <td>1,357.80</td>
+                              <td>3.72</td>
+                            </tr>
+                            <tr>
+                              <td>OPL248777</td>
+                              <td>TestGiven</td>
+                              <td>TestLast</td>
+                              <td>M</td>
+                              <td>1980-05-03</td>
+                              <td>14 Asdf fdsfsdf</td>
+                              <td>Toronto</td>
+                              <td>ON</td>
+                              <td>M2M 4M5</td>
+                              <td>9283</td>
+                              <td>458286</td>
+                              <td>2016-06-05</td>
+                              <td>2016-05-05</td>
+                              <td>2016-08-03</td>
+                              <td>2017-08-02</td>
+                              <td>365</td>
+                              <td>10,000</td>
+                              <td>733.21</td>
+                              <td>1,357.80</td>
+                              <td>3.72</td>
+                            </tr>
+                            <tr>
+                              <td colspan="22">JF Royal Visitor to Canada</td>
+                            </tr>
+                            <tr>
+                              <td>JFR248775</td>
+                              <td>TestGiven</td>
+                              <td>TestLast</td>
+                              <td>M</td>
+                              <td>1980-05-03</td>
+                              <td>14 Asdf fdsfsdf</td>
+                              <td>Toronto</td>
+                              <td>ON</td>
+                              <td>M2M 4M5</td>
+                              <td>9283</td>
+                              <td>458286</td>
+                              <td>2016-06-05</td>
+                              <td>2016-05-05</td>
+                              <td>2016-08-03</td>
+                              <td>2017-08-02</td>
+                              <td>365</td>
+                              <td>10,000</td>
+                              <td>733.21</td>
+                              <td>1,357.80</td>
+                              <td>3.72</td>
+                            </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End List Section -->
 
           </div>
         </div>
