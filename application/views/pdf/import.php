30e093739bed1c07074baf50207fac6b8a3fb519
zoewen <zoeywenyue@gmail.com>
2016-07-04 14:14:42 -0400

    detailed

--- application/views/pdf/import.php
+++ application/views/pdf/import.php
@@ -36,44 +36,56 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 					</div>
 					<div class="x_content">
 						<br />
-<?php if (!empty($errormsg)) { ?>
-<?php echo $errormsg; ?>
-<?php } ?>
-<?php if (!empty($successmsg)) { ?>
-<?php echo $successmsg; ?>
-<?php } ?>
-          					<form action="<?php $action_url; ?>" method="post" enctype="multipart/form-data" class="form-horizontal form-label-left">
+
+						<?php if (!empty($errormsg)) { ?>
+						<div class="alert-error">
+							<?php echo $errormsg; ?><br />
+						</div>
+						<?php } ?>
+						<?php if (!empty($successmsg)) { ?>
+						<div class="alert-error">
+							<?php echo $successmsg; ?><br />
+						</div>
+						<?php } ?>
+          				<form action="<?php $action_url; ?>" method="post" enctype="multipart/form-data" class="form-horizontal form-label-left">
 							<input type='hidden' name='<?php echo $csrf['name']; ?>' value='<?php echo $csrf['value']; ?>'>
-							<div class="form-group">
-								<label class="col-sm-12">School</label>
-								<div class="col-sm-12">
+							<div class="row">
+								<div class="form-group col-sm-4">
+									<label class="col-sm-12">School</label>
 									<select class="form-control" name='user_id'>
-										<option value=''>Choose School</option>
-<?php foreach ($schools as $key => $value) { ?>                            
-                            			<option value='<?php echo $key?>' <?php echo ($user_id == $key) ? 'selected' : ''; ?>><?php echo $value; ?></option>
-<?php } ?>
-                          			</select>
+										<option value=''>-- Choose School --</option>
+										<?php foreach ($schools as $key => $value) { ?>                            
+		                            	<option value='<?php echo $key?>' <?php echo ($user_id == $key) ? 'selected' : ''; ?>><?php echo $value; ?></option>
+										<?php } ?>
+		                          	</select>
 								</div>
-							</div>
-							<div class="form-group">
-								<label class="col-sm-12">Insurance Product</label>
-								<div class="col-sm-12">
+								<div class="form-group col-sm-4">
+									<label class="col-sm-12">Insurance Product</label>
 									<select class="form-control" name='product_short'>
-										<option value=''>Choose Product</option>
-<?php foreach ($products as $key => $value) { ?>                            
-                            			<option value='<?php echo $key?>' <?php echo ($product_id == $key) ? 'selected' : ''; ?>><?php echo $value['full_name']; ?></option>
-<?php } ?>
-                          			</select>
+										<option value=''>-- Choose Product --</option>
+										<?php foreach ($products as $key => $value) { ?>                            
+		                           		<option value='<?php echo $key?>' <?php echo ($product_id == $key) ? 'selected' : ''; ?>><?php echo $value['full_name']; ?></option>
+										<?php } ?>
+		                        	</select>
 								</div>
 							</div>
 							<hr />
-
-							<div class="form-group">
-								<div class="col-sm-12">
-									<input style="display: inline-block; vertical-align: middle;" type="file" name="userfile" size="20" />
-									<input style="display: inline-block; vertical-align: middle;" type="submit" name='submit' value="upload" />
+							<div class="row">
+								<div class="form-group col-sm-8">
+									<input id="uploadFile" placeholder="Choose File" />
+									<div class="fileUpload btn btn-primary">
+										<span>Browse</span>
+										<input id="uploadBtn" type="file" class="upload" name="userfile" size="20"/>
+									</div>
+									<script>
+										document.getElementById("uploadBtn").onchange = function () {
+										document.getElementById("uploadFile").value = this.value;
+									};
+									</script>
+									<input style="display: inline-block; vertical-align: bottom;" class="btn btn-primary" type="submit" name='submit' value="upload" />
 								</div>
 							</div>
+							
 						</form>
 					</div>
 				</div>
