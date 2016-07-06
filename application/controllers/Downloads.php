e6ac71edd0bf3b8204d5bba4bf7ed90bd9080e01
zoewen <zoeywenyue@gmail.com>
2016-07-06 17:48:51 -0400

    detail

--- application/controllers/Downloads.php
+++ application/controllers/Downloads.php
@@ -10,6 +10,28 @@ class Downloads extends MY_Controller {
 	{
 		$data['title_txt'] = 'Downloads';
 		$data['downloads_url'] = base_url('pdf/download') . "/";
+		
+		$data['url_brochure'] = '_Brochure';
+		$data['url_benefit'] = '_Benefit_Summary';
+		$data['url_claimf'] = '_Claim_Form';
+		$data['url_claimp'] = '_Claim_Procedure';
+		$data['url_consent'] = '_Consent_Form';
+		$data['url_policy'] = '_Policy';
+
+		$data['text_brochure'] = 'Brochure';
+		$data['text_benefit'] = 'Benefit Summary';
+		$data['text_claimf'] = 'Claim Form';
+		$data['text_claimp'] = 'Claim Procedure';
+		$data['text_consent'] = 'Consent Form';
+		$data['text_policy'] = 'Policy';
+
+		$data['opl'] = 'OPL_Visitor';
+		$data['nus'] = 'NUS';
+		$data['jus'] = 'JUS';
+		$data['jfss'] = 'JFSS';
+		$data['jfr'] = 'JFR';
+		$data['jfep'] = 'JF_Elite_Plus';
+
 		$data['top_menu'] = $this->menu_model->load_top_menu();
 		$data['menu'] = $this->menu_model->load_meun();
 
--- application/views/downloads.php
+++ application/views/downloads.php
@@ -37,17 +37,84 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                  	<div class="row">
+                  	<div class="row dfile-list">
                   		<div class="col-sm-12">
                   			<label>JF Optimum Plus</label>
                   		</div>
                   		<div class="col-sm-12">
-                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. 'JF_Elite_Plus_Benefit_Summary.pdf' ;?>">Brochure</a>
-                  			<a class="d_benifit" target="_blank" href="">Benefit Summary</a>
-                  			<a class="d_claimf" target="_blank" href="">Claim Form</a>
-                  			<a class="d_claimp" target="_blank" href="">Claim Procedure</a>
-                  			<a class="d_consent" target="_blank" href="">Consent Form</a>
-                  			<a class="d_policy" target="_blank" href="">Policy</a>
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $opl . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="d_benefit" target="_blank" href="<?php echo $downloads_url. $opl . $url_benefit . '.pdf';?>"><?php echo $text_benefit;?></a>
+                  			<a class="d_claimf" target="_blank" href="<?php echo $downloads_url. $opl . $url_claimf . '.pdf';?>"><?php echo $text_claimf;?></a>
+                  			<a class="d_claimp" target="_blank" href="<?php echo $downloads_url. $opl . $url_claimp . '.pdf';?>"><?php echo $text_claimp;?></a>
+                  			<a class="d_consent" target="_blank" href="<?php echo $downloads_url. $opl . $url_consent . '.pdf';?>"><?php echo $text_consent;?></a>
+                  			<a class="d_policy" target="_blank" href="<?php echo $downloads_url. $opl . $url_policy . '.pdf';?>"><?php echo $text_policy;?></a>
+                  		</div>
+                  	</div>
+                  	<div class="row dfile-list">
+                  		<div class="col-sm-12">
+                  			<br />
+                  			<label>JF Royal Visitor to Canada</label>
+                  		</div>
+                  		<div class="col-sm-12">
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $jfr . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="d_benefit" target="_blank" href="<?php echo $downloads_url. $jfr . $url_benefit . '.pdf';?>"><?php echo $text_benefit;?></a>
+                  			<a class="d_claimf" target="_blank" href="<?php echo $downloads_url. $jfr . $url_claimf . '.pdf';?>"><?php echo $text_claimf;?></a>
+                  			<a class="d_claimp" target="_blank" href="<?php echo $downloads_url. $jfr . $url_claimp . '.pdf';?>"><?php echo $text_claimp;?></a>
+                  			<a class="d_consent" target="_blank" href="<?php echo $downloads_url. $jfr . $url_consent . '.pdf';?>"><?php echo $text_consent;?></a>
+                  			<a class="d_policy" target="_blank" href="<?php echo $downloads_url. $jfr . $url_policy . '.pdf';?>"><?php echo $text_policy;?></a>
+                  		</div>
+                  	</div>
+                  	<div class="row dfile-list">
+                  		<div class="col-sm-12">
+                  			<br />
+                  			<label>JFUSA</label>
+                  		</div>
+                  		<div class="col-sm-12">
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $jus . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_benefit;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_claimf;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_claimp;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_consent;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_policy;?></a>
+                  		</div>
+                  	</div>
+                  	<div class="row dfile-list">
+                  		<div class="col-sm-12">
+                  			<br />
+                  			<label>NihaoUSA</label>
+                  		</div>
+                  		<div class="col-sm-12">
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $nus . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_benefit;?></a>
+                  			<a class="nofile" target="_blank"><?php echo $text_claimf;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_claimp;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_consent;?></a>
+                  			<a class="nofile" target="_blank" ><?php echo $text_policy;?></a>
+                  		</div>
+                  	</div>
+                  	<div class="row dfile-list">
+                  		<div class="col-sm-12">
+                  			<br />
+                  			<label>JF Elite Plus International Student to Canada</label>
+                  		</div>
+                  		<div class="col-sm-12">
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $jfep . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="d_benefit" target="_blank" href="<?php echo $downloads_url. $jfep . $url_benefit . '.pdf';?>"><?php echo $text_benefit;?></a>
+                  			<a class="d_claimf" target="_blank" href="<?php echo $downloads_url. $jfep . $url_claimf . '.pdf';?>"><?php echo $text_claimf;?></a>
+                  			<a class="d_policy" target="_blank" href="<?php echo $downloads_url. $jfep . $url_policy . '.pdf';?>"><?php echo $text_policy;?></a>
+                  		</div>
+                  	</div>
+                  	<div class="row dfile-list">
+                  		<div class="col-sm-12">
+                  			<br />
+                  			<label>Study Safe International Student to Canada</label>
+                  		</div>
+                  		<div class="col-sm-12">
+                  			<a class="d_brochure" target="_blank" href="<?php echo $downloads_url. $opl . $url_brochure . '.pdf';?>"><?php echo $text_brochure;?></a>
+                  			<a class="d_claimf" target="_blank" href="<?php echo $downloads_url. $opl . $url_claimf . '.pdf';?>"><?php echo $text_claimf;?></a>
+                  			<a class="d_claimp" target="_blank" href="<?php echo $downloads_url. $opl . $url_claimp . '.pdf';?>"><?php echo $text_claimp;?></a>
+                  			<a class="d_consent" target="_blank" href="<?php echo $downloads_url. $opl . $url_consent . '.pdf';?>"><?php echo $text_consent;?></a>
+                  			<a class="d_policy" target="_blank" href="<?php echo $downloads_url. $opl . $url_policy . '.pdf';?>"><?php echo $text_policy;?></a>
                   		</div>
                   	</div>
                     
