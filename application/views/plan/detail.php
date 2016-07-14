b75bf1ad21dcb9fd5343ff54b88f429684a3c610
Jack1232 <wqjyhggg@gmail.com>
2016-07-14 12:10:36 -0400

    fix

--- application/views/plan/detail.php
+++ application/views/plan/detail.php
@@ -226,7 +226,7 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 								<?php if (($plan['status_id'] <= 2) && empty($defaultpay_type)) { ?>						
 									<button class="btn btn-primary pull-right" data-toggle="collapse" data-target="#payment-div">Comfirm and Pay</button>		
 								<?php } ?>
-								<a class="btn btn-info pull-right" target="_blank" href='<?php echo $pdf_url . $plan['plan_id']; ?>'>Export PDF</a>
+								<a class="btn btn-info pull-right" target="_blank" href='<?php echo $pdf_url; ?>'>Export PDF</a>
 
 								</div>
 						</div>
