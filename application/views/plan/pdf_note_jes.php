d327dbbea469dba980a660961bfd489462fb7479
zoewen <zoeywenyue@gmail.com>
2016-07-22 17:08:25 -0400

    plan pdf detail

---
+++ application/views/plan/pdf_note_jes.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<div class="row">
+		<div class="col-sm-12 nopm">
+			<p class="small">All persons insured are subject to the terms and conditions below.
+			<br />This document does not constitute the entire insurance policy, and the application(s) has been advised to read the policy full details of converge and exclusions.
+			<br />. The eligibility requirements for the plan applied for are material to the risk for which insurance is sought. If the application(s) does not meet the eligibility requirements for the plan selected, or if there is any misrepresentation of concealment of or failure to disclose any facts or matters pertaining to the application(s) and that are the subject of the application form, then there shall be no insurance coverage provided.
+			<br />. The application(s) is aware that this insurance covers Emergencies (as defined in the policy) and may not cover expanses incurred after the application(s) is able to travel home for treatment. The application(s) is aware that Pre-existing Conditions (as defined in the policy) are excluded in some circumstances and that further details are provided in the policy.
+			<br />IN THE EVENT OF A MEDICAL EMERGENCY OR CLAIM THAT MAY REQUIRE OR RESULT IN HOSPITALIZATION, CALL OTC IMMEDIATELY
+			<br />From Canada & USA 1-888-988-3268
+			<br />Collect call worldwide 905-918-0188</p>
+				
+		</div>
+	</div><br />
