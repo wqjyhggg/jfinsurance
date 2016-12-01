1dd66561c23bb9de1064ea48a6fbc6bbd7a44173
Jack1232 <wqjyhggg@gmail.com>
2016-12-01 15:09:47 -0500

    fix

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -692,7 +692,8 @@ class Report_model extends CI_Model
         $this->common_from();
         $this->db->join('user_product up', 'u.user_id = up.user_id and pr.product_short = up.product_short', 'left');
         $this->db->join('payment pa', 'pl.plan_id = pa.plan_id AND pa.pay_type = "commission"');
-        $this->db->join('payment pa2', 'pa.plan_id = pa2.plan_id AND pa2.pay_type = "premium" AND ABS(TIMESTAMPDIFF(SECOND, pa2.added, pa.added)) <5');
+        $this->db->join('payment pa2', 'pa.plan_id = pa2.plan_id AND pa2.pay_type = "premium" AND ABS(pa2.payment_id - pa.payment_id) <3');
+//        $this->db->join('payment pa2', 'pa.plan_id = pa2.plan_id AND pa2.pay_type = "premium" AND ABS(TIMESTAMPDIFF(SECOND, pa2.added, pa.added)) <5');
     }
 
     private function commission_where($para)
