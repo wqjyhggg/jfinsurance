1ff708e0f258b44f2f5922249fba4884ddcd2b52
zzzoe <zoeywenyue@gmail.com>
2017-01-11 13:00:10 -0500

    detail fix

--- application/views/plan/pdf_note_jes.php
+++ application/views/plan/pdf_note_jes.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 	<div class="row">
 		<div class="col-sm-12 nopm">
-			<p class="small">All persons insured are subject to the terms and conditions below.
+			<p class="small" style="margin-top:2px;">All persons insured are subject to the terms and conditions below.
 			<br />This document does not constitute the entire insurance policy, and the application(s) has been advised to read the policy full details of converge and exclusions.
 			<br />. The eligibility requirements for the plan applied for are material to the risk for which insurance is sought. If the application(s) does not meet the eligibility requirements for the plan selected, or if there is any misrepresentation of concealment of or failure to disclose any facts or matters pertaining to the application(s) and that are the subject of the application form, then there shall be no insurance coverage provided.
 			<br />. The application(s) is aware that this insurance covers Emergencies (as defined in the policy) and may not cover expanses incurred after the application(s) is able to travel home for treatment. The application(s) is aware that Pre-existing Conditions (as defined in the policy) are excluded in some circumstances and that further details are provided in the policy.
