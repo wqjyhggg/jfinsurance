e2fea8c6d23381c1dec98b95c66e54a3d5d4f879
Jack1232 <wqjyhggg@gmail.com>
2017-04-24 11:16:49 -0400

    adjust

--- application/views/reports/commission_pdf.php
+++ application/views/reports/commission_pdf.php
@@ -2,7 +2,7 @@
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 ?>
 <?php if ($data ['agent'] ['receive_type'] == 'Cheque') { ?>
-		<table width='100%' style="display: block; font-family: serif; font-size: 10pt; padding: 40mm 0mm 25mm 10mm;">
+		<table width='100%' style="display: block; font-family: serif; font-size: 12pt; padding: 40mm 0mm 25mm 10mm;">
 			<tbody>
 				<tr>
 					<td>
@@ -20,6 +20,7 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 				</tr>
 			</tbody>
 		</table>
+		<hr />
 <?php } ?>
 		<table width='100%' style="display: block; font-family: serif; font-size: 10pt;">
 			<tbody>
