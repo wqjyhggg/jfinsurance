94a30c148dfdc020957b01e624e5ace6360dac6a
Ubuntu <ubuntu@ip-172-31-51-106.ec2.internal>
2017-07-07 10:02:24 -0400

    disable report for agent 2202

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -119,8 +119,8 @@ class Menu_model extends CI_Model {
     		}
     	}
     	$menu = $meunArr[$group];
-    	if ($user && ($user['user_id'] == 2202)) {
-    		unset($menu[1]);
+    	if ($beuser && ($beuser['user_id'] == 2202)) {
+    		unset($menu[2]);
     	}
     	if ($user && ($user['user_group_id'] != 105) && ($user['user_group_id'] != 103) && ($user['user_group_id'] != 3 )) {
 			if ($onbehalf) {
