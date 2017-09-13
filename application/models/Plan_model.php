696088efb10266fc48f4c552be9b27119221afe6
Ubuntu <ubuntu@ip-172-31-10-137.ec2.internal>
2017-09-13 20:49:29 +0000

    fix

--- application/models/Plan_model.php
+++ application/models/Plan_model.php
@@ -316,7 +316,14 @@ class Plan_model extends CI_Model {
 		}
 		$plan_id = $plan['plan_id'];
 		$this->logstr .= "Change Plan (" . (int)$plan_id . "): " . ($isvsuser ? ' by customer ' : ' ');
-		$isfamilyplan = isset($para['isfamilyplan']) ? ((((int)$para['isfamilyplan']) > 1) ? (int)$para['isfamilyplan'] : 1) : 0;
+		$isfamilyplan = 0;
+		if (isset($para['isfamilyplan'])) {
+			if ((int)$para['isfamilyplan'] >= 1) {
+				$isfamilyplan = (int)$para['isfamilyplan'];
+			} else if (!empty($para['isfamilyplan'])) {
+				$isfamilyplan = 1;
+			}
+		}
 		$holiday_rate = empty($para['holiday_rate']) ? 0 : 1;
 		$spouse = empty($para['spouse']) ? 0 : 1;
 	
