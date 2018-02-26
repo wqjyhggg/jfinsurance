1834f341402a385ce060d187ce833a1ddd73b083
Jack1232 <wqjyhggg@gmail.com>
2018-02-26 12:03:52 -0500

    fix

--- application/models/Myhome_model.php
+++ application/models/Myhome_model.php
@@ -188,82 +188,83 @@ class Myhome_model extends CI_Model {
 			$sql = "INSERT INTO myhome SET last_update=NOW(), user_id=" . (int)$para['user_id'];
 		}
 		
-		if (!empty($para['myname']) && !($my && ($my['myname'] == $para['myname']))) {
+		if (isset($para['myname']) && (empty($my) || ($my['myname'] != $para['myname']))) {
 			$sql .= ", myname=" . $this->db->escape($para['myname']);
 			$this->logstr .= 'agentname=>'.$para['myname']."; ";
 		}
-		if (!empty($para['logo']) && !($my && ($my['logo'] == $para['logo']))) {
+		if (isset($para['logo']) && (empty($my) || ($my['logo'] != $para['logo']))) {
 			$sql .= ", logo=" . $this->db->escape($para['logo']);
 			$this->logstr .= 'logo=>'.$para['logo']."; ";
 		}
-		if (!empty($para['image']) && !($my && ($my['image'] == $para['image']))) {
+		if (isset($para['image']) && (empty($my) || ($my['image'] != $para['image']))) {
 			$sql .= ", image=" . $this->db->escape($para['image']);
 			$this->logstr .= 'image=>'.$para['image']."; ";
 		}
-		if (!empty($para['qr']) && !($my && ($my['qr'] == $para['qr']))) {
+		if (isset($para['qr']) && (empty($my) || ($my['qr'] != $para['qr']))) {
 			$sql .= ", qr=" . $this->db->escape($para['qr']);
 			$this->logstr .= 'qr=>'.$para['qr']."; ";
 		}
-		if (!empty($para['qr_desc']) && !($my && ($my['qr_desc'] == $para['qr_desc']))) {
+		if (isset($para['qr_desc']) && (empty($my) || ($my['qr_desc'] != $para['qr_desc']))) {
 			$sql .= ", qr_desc=" . $this->db->escape($para['qr_desc']);
 			$this->logstr .= 'qr_desc=>'.$para['qr_desc']."; ";
 		}
-		if (!empty($para['top_title']) && !($my && ($my['top_title'] == $para['top_title']))) {
+		if (isset($para['top_title']) && (empty($my) || ($my['top_title'] != $para['top_title']))) {
 			$sql .= ", top_title=" . $this->db->escape($para['top_title']);
 			$this->logstr .= 'top_title=>'.$para['top_title']."; ";
 		}
-		if (!empty($para['top_desc']) && !($my && ($my['top_desc'] == $para['top_desc']))) {
+		if (isset($para['top_desc']) && (empty($my) || ($my['top_desc'] != $para['top_desc']))) {
 			$sql .= ", top_desc=" . $this->db->escape($para['top_desc']);
 			$this->logstr .= 'top_desc=>'.$para['top_desc']."; ";
 		}
-		if (!empty($para['about_title']) && !($my && ($my['about_title'] == $para['about_title']))) {
+		if (isset($para['about_title']) && (empty($my) || ($my['about_title'] != $para['about_title']))) {
 			$sql .= ", about_title=" . $this->db->escape($para['about_title']);
 			$this->logstr .= 'about_title=>'.$para['about_title']."; ";
 		}
-		if (!empty($para['about_short']) && !($my && ($my['about_short'] == $para['about_short']))) {
+		if (isset($para['about_short']) && (empty($my) || ($my['about_short'] != $para['about_short']))) {
 			$sql .= ", about_short=" . $this->db->escape($para['about_short']);
 			$this->logstr .= 'about_short=>'.$para['about_short']."; ";
 		}
-		if (!empty($para['about_desc']) && !($my && ($my['about_desc'] == $para['about_desc']))) {
+		if (isset($para['about_desc']) && (empty($my) || ($my['about_desc'] != $para['about_desc']))) {
 			$sql .= ", about_desc=" . $this->db->escape($para['about_desc']);
 			$this->logstr .= 'about_desc=>'.$para['about_desc']."; ";
 		}
-		if (!empty($para['foot_title']) && !($my && ($my['foot_title'] == $para['foot_title']))) {
+		if (isset($para['foot_title']) && (empty($my) || ($my['foot_title'] != $para['foot_title']))) {
 			$sql .= ", foot_title=" . $this->db->escape($para['foot_title']);
 			$this->logstr .= 'foot_title=>'.$para['foot_title']."; ";
 		}
-		if (!empty($para['address']) && !($my && ($my['address'] == $para['address']))) {
+		if (isset($para['address']) && (empty($my) || ($my['address'] != $para['address']))) {
 			$sql .= ", address=" . $this->db->escape($para['address']);
 			$this->logstr .= 'address=>'.$para['address']."; ";
 		}
-		if (!empty($para['city_province']) && !($my && ($my['city_province'] == $para['city_province']))) {
+		if (isset($para['city_province']) && (empty($my) || ($my['city_province'] != $para['city_province']))) {
 			$sql .= ", city_province=" . $this->db->escape($para['city_province']);
 			$this->logstr .= 'city_province=>'.$para['city_province']."; ";
 		}
-		if (!empty($para['post_code']) && !($my && ($my['post_code'] == $para['post_code']))) {
+		if (isset($para['post_code']) && (empty($my) || ($my['post_code'] != $para['post_code']))) {
 			$sql .= ", post_code=" . $this->db->escape($para['post_code']);
 			$this->logstr .= 'post_code=>'.$para['post_code']."; ";
 		}
-		if (!empty($para['phone']) && !($my && ($my['phone'] == $para['phone']))) {
+		if (isset($para['phone']) && (empty($my) || ($my['phone'] != $para['phone']))) {
 			$sql .= ", phone=" . $this->db->escape($para['phone']);
 			$this->logstr .= 'phone=>'.$para['phone']."; ";
 		}
-		if (!empty($para['fax']) && !($my && ($my['fax'] != $para['fax']))) {
+		if (isset($para['fax']) && (empty($my) || ($my['fax'] != $para['fax']))) {
 			$sql .= ", fax=" . $this->db->escape($para['fax']);
 			$this->logstr .= 'fax=>'.$para['fax']."; ";
 		}
-		if (!empty($para['toll_free']) && !($my && ($my['toll_free'] == $para['toll_free']))) {
+		if (isset($para['toll_free']) && (empty($my) || ($my['toll_free'] != $para['toll_free']))) {
 			$sql .= ", toll_free=" . $this->db->escape($para['toll_free']);
 			$this->logstr .= 'toll_free=>'.$para['toll_free']."; ";
 		}
-		if (!empty($para['toll_free_fax']) && !($my && ($my['toll_free_fax'] == $para['toll_free_fax']))) {
+		if (isset($para['toll_free_fax']) && (empty($my) || ($my['toll_free_fax'] != $para['toll_free_fax']))) {
 			$sql .= ", toll_free_fax=" . $this->db->escape($para['toll_free_fax']);
 			$this->logstr .= 'toll_free_fax=>'.$para['toll_free_fax']."; ";
 		}
-		if (!empty($para['email']) && !($my && ($my['email'] == $para['email']))) {
+		if (isset($para['email']) && (empty($my) || ($my['email'] != $para['email']))) {
 			$sql .= ", email=" . $this->db->escape($para['email']);
 			$this->logstr .= 'email=>'.$para['email']."; ";
 		}
+
 		$this->db->query($sql . $where);
 		$this->sqlstr = $this->db->last_query();
 		$last_id = $this->db->insert_id();
