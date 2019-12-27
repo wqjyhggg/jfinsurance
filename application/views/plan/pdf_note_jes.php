7b57026d25352d24886328d866135e5748ed4b9b
Ubuntu <ubuntu@ip-172-31-2-246.ec2.internal>
2019-12-27 18:24:56 +0000

    change worlding

--- application/views/plan/pdf_note_jes.php
+++ application/views/plan/pdf_note_jes.php
@@ -3,13 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 	<div class="row">
 		<div class="col-sm-12 nopm">
-			<p class="small" style="margin-top:2px;">All persons insured are subject to the terms and conditions below.
-			<br />This document does not constitute the entire insurance policy, and the application(s) has been advised to read the policy full details of converge and exclusions.
-			<br />. The eligibility requirements for the plan applied for are material to the risk for which insurance is sought. If the application(s) does not meet the eligibility requirements for the plan selected, or if there is any misrepresentation of concealment of or failure to disclose any facts or matters pertaining to the application(s) and that are the subject of the application form, then there shall be no insurance coverage provided.
-			<br />. The application(s) is aware that this insurance covers Emergencies (as defined in the policy) and may not cover expanses incurred after the application(s) is able to travel home for treatment. The application(s) is aware that Pre-existing Conditions (as defined in the policy) are excluded in some circumstances and that further details are provided in the policy.
-			<br />IN THE EVENT OF A MEDICAL EMERGENCY OR CLAIM THAT MAY REQUIRE OR RESULT IN HOSPITALIZATION, CALL OTC IMMEDIATELY
-			<br />From Canada & USA 1-888-784-9608
-			<br />Collect call worldwide 905-707-9555</p>
-				
-		</div>
-	</div><br />
+
+		<p class="small" style="margin-top:2px;">Underwritten by CUMIS General Insurance Company, a member of The Co-operators group of companies.</p>
+
+		<p class="small" style="margin-top:2px;">All persons insured are subject to the terms and conditions below.</p>
+
+		<p class="small" style="margin-top:2px;">This document does not constitute the entire insurance policy, and the insured(s) has been advised to read the full policy details of coverage and exclusions.</p>
+
+		<p class="small" style="margin-top:2px;">The eligibility requirements for the plan applied for are material to the risk for which insurance is sought. If the insured(s) does not meet the eligibility requirements for the plan selected, or if there is any misrepresentation, concealment, or failure to disclose any facts or matters pertaining to the insured(s) that is the subject of the application form, then there shall be no insurance coverage provided.</p>
+
+		<p class="small" style="margin-top:2px;">The insured(s) is aware that this insurance covers Emergencies (as defined in the policy) and may not cover expenses incurred after the insured(s) is able to travel home for treatment. The insured(s) is aware that Pre-existing Conditions (as defined in the policy) are excluded in some circumstances and that further details are provided in the policy.</p>
+		<p class="small" style="margin-top:2px;">IN THE EVENT OF A MEDICAL EMERGENCY OR CLAIM THAT MAY REQUIRE OR RESULT IN HOSPITALIZATION, CALL ONTIME CARE WORLDWIDE INC. IMMEDIATELY</p>
+		<p class="small" style="margin-top:2px;">Toll Free Canada/U.S.A. 1-866-209-5804<br />
+			Collect call worldwider 905-707-9555<br />
+			If you notice any errors in the above information or have any questions, please contact JF Insurance Agency Group Inc. .</p>
+
+	</div>
+</div><br />
