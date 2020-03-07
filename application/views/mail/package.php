80e443e75a9cd230837c8446de6f3aad8aa5c85d
Your Name <you@example.com>
2020-03-07 12:17:26 -0500

    for change email template

--- application/views/mail/package.php
+++ application/views/mail/package.php
@@ -1,19 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 ?>
-<h3>*PLEASE DO NOT REPLY TO THIS E-MAIL*</h3>
-
-<p style="color:red;">Please note before policy effective, if you do not get your visa or will change your trip, please inform your agent immediately.</p>
-<p style="color:red;">请注意:在保单生效之前, 若您未能取得签证或改变行程日期,请务必通知您的Agent.</p>
 <br />
-<p>Dear Customer,</p>
-<p>Thank You for purchasing medical insurance through Johnson Fu Insurance Agency Inc.</p>
-<p>Due to the protection to our environment and the efficiency of delivering policies, our company will no longer send out the package through mail.</p>
-<p>All the confirmations and policy wording will be sent out through e-mail.</p>
+<p>Dear <? echo $customer['firstname']; ?>,</p>
+<p>Thank you for purchasing medical insurance from JF Insurance Agency Group Inc.</p>
 <br />
-<p>Please refer to the attached documents for complete details of your coverage.</p>
-<p>Should you have any trouble viewing the attached document, please download the latest version of Adobe Reader at the following location: http://get.adobe.com/reader.</p>
+<p>Please refer to the attached documents for your policy confirmation, details of your coverage and claim form. Please refer to the claim form for procedures on how to claim.</p>
+<br />
+<p>If you notice any errors in your policy confirmation or need to change your trip before the effective date, please inform the agent/school/agency you purchased the insurance from immediately.</p>
+<br />
+<p>Should you have any trouble viewing the attached document, please download the latest version of Adobe Reader : http://get.adobe.com/reader.</p>
 <p></p>
 <p></p>
 <br />
@@ -23,6 +19,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <p>15 Wertheim Court, Suite #501</p>
 <p>Richmond Hill, ON L4B 3H7</p>
 <p>Tel: 905-707-1512 Fax: 905-707-1513</p>
+<p>Email: Info@jfgroup.ca</p>
 <p>Website: www.jfgroup.ca</p>
-
-<?php
