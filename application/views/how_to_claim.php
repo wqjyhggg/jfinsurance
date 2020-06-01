6aa5e327990cadd7a45fff5ec0dbb4af14511a0a
Jack1232 <wqjyhggg@gmail.com>
2020-06-01 19:19:19 -0400

    add guo and mp4 for how_to_claim page

--- application/views/how_to_claim.php
+++ application/views/how_to_claim.php
@@ -1,61 +1,42 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
 <div class="container">
 	<div class="row">
 		<div class="col-sm-12">
-			<div class="main" style="min-height:800px;">
-Ontime Care logo
-
- 
-
-Ontime Care Worldwide Inc. Is the authorized claims administrator for JF insurance policies (JES, JFP, JFC, OPL, JFR, TOP)
-
- 
-
-There are two ways to submit your claim:
-
-1.      By Mail: 
-
-Mail your completed claim form (sent with your insurance policy confirmation), original receipts, and supporting medical reports to:
-
-15 Wertheim Court, Suite 512, Richmond Hill, ON, L4B 3H7
-
-Please ensure to keep a copy of your claim for your own records.
-
- 
-
-2.      Online:
-
-Web claim submissions are accepted for claims with total expenses under $500. (For claims over $500, please mail in your claim).
-
-Please login below to submit your claim.
-
-Submitting online is easy! – see the video for step by step process.
-
- 
-
-Please READ before you submit your claim online:
-
-·       You can submit your claims online if your policy number begins with the following:  JES, JFP, JFC, OPL, JFR, TOP.
-
-·       Please submit one claim for each medical condition (Example/ Doctor visit and pharmacy expense for the same condition would fall under one claim).​
-
-·       Please attach pdf files or readable photos of all itemized invoices and payment receipts from the medical service provider, pharmacy or hospital along with any detailed medical reports. ​
-
-·       For patients treated at a medical clinic, a physician’s medical report is required. ​
-
-·       Failure to include the required supporting documentation will delay processing.​
-
-·       Claim must be submitted to OTC within 90 days of occurrence.​
-
-·       You are responsible for all fees charged for any supporting documentation with your claims.
-
-·       We reserve the right to request submission of the original documentation or additional information if needed.				
-
-			</div>
+			<img src="<?php echo base_url(); ?>image/otc_logo.jpg" style="max-width:200px; padding: 20px; display: block; margin-left: 5%;">
 		</div>
 	</div>
-</div>
-mp4 URL: <a href="/eclaim.mp4"> Video </a>
+	<div class="row">
+		<div class="col-sm-12" style="padding: 1rem 5% 5rem 5%;">
+			<p>Ontime Care Worldwide Inc. Is the authorized claims administrator for JF insurance policies (JES, JFP, JFC, OPL, JFR, TOP)</p>
+			<p style="text-decoration: underline;">There are two ways to submit your claim:</p>
+			<p>
+				<b>1. By Mail: </b>
+				<p>Mail your completed claim form (sent with your insurance policy confirmation), original receipts, and supporting medical reports to:</p>
+				<p><I>15 Wertheim Court, Suite 512, Richmond Hill, ON, L4B 3H7</I></p>
+				<p>Please ensure to keep a copy of your claim for your own records.</p>
+			</p>
+			<p>
+				<b>2. Online: </b>
+				<p>Web claim submissions are accepted for claims with total expenses under $500. (For claims over $500, please mail in your claim).</p>
+				<p>Please login below to submit your claim.</p>
+				<p>Submitting online is easy! – <a style="text-decoration: underline;" target="_blank" href="<?php echo base_url(); ?>/eclaim.mp4">video for step by step process</a>.</p>
+			</p>
+			<p style="text-decoration: underline; font-weight: bold;">Please READ before you submit your claim online:</p>
+			<p>
+				<ul>
+					<li>You can submit your claims online if your policy number begins with the following: JES, JFP, JFC, OPL, JFR, TOP.</li>
+					<li>Please submit one claim for each medical condition (Example/ Doctor visit and pharmacy expense for the same condition would fall under one claim).​</li>
+					<li>Please attach pdf files or readable photos of all itemized invoices and payment receipts from the medical service provider, pharmacy or hospital along with any detailed medical reports. ​</li>
+					<li>For patients treated at a medical clinic, a physician’s medical report is required. ​</li>
+					<li>Failure to include the required supporting documentation will delay processing.​</li>
+					<li>Claim must be submitted to OTC within 90 days of occurrence.​</li>
+					<li>You are responsible for all fees charged for any supporting documentation with your claims.</li>
+					<li>We reserve the right to request submission of the original documentation or additional information if needed.</li>
+				</ul>
+			</p>
+		</div>
+	</div>
+s</div>
