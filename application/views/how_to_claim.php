d0c904ee11165185e7761be21e3d04c54bd0efee
Ubuntu <ubuntu@ip-172-31-51-106.ec2.internal>
2020-06-08 12:37:40 -0400

    remove links from how to claim page

--- application/views/how_to_claim.php
+++ application/views/how_to_claim.php
@@ -10,29 +10,29 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	</div>
 	<div class="row">
 		<div class="col-sm-12" style="padding: 1rem 5% 5rem 5%;">
-			<p>Ontime Care Worldwide Inc. Is the authorized claims administrator for JF insurance policies (JES, JFP, JFC, OPL, JFR, TOP)</p>
+			<p>Ontime Care Worldwide Inc. is the authorized claims administrator for JF insurance policies (JES, JFP, JFC, OPL, JFR, TOP)</p>
 			<p style="text-decoration: underline;">There are two ways to submit your claim:</p>
 			<p>
 				<b>1. By Mail: </b>
 				<p>Mail your completed claim form (sent with your insurance policy confirmation), original receipts, and supporting medical reports to:</p>
-				<p><I>15 Wertheim Court, Suite 512, Richmond Hill, ON, L4B 3H7</I></p>
+				<p><I>Ontime Care Worldwide Inc.<br />15 Wertheim Court, Suite 512, Richmond Hill, ON, L4B 3H7</I></p>
 				<p>Please ensure to keep a copy of your claim for your own records.</p>
 			</p>
 			<p>
 				<b>2. Online: </b>
 				<p>Web claim submissions are accepted for claims with total expenses under $500. (For claims over $500, please mail in your claim).</p>
-				<p>Please login below to submit your claim.</p>
-				<p><a style="text-decoration: underline;" href="https://eclaim.jfgroup.ca">Submitting online</a> is easy! – <a style="text-decoration: underline;" target="_blank" href="<?php echo base_url(); ?>/eclaim.mp4">video for step by step process</a>.</p>
+				<p>Please go to <a style="text-decoration: underline;" href="https://eclaim.jfgroup.ca">eclaim.jfgroup.ca</a> to submit your claim.</p>
+				<p>Submitting online is easy! – <a style="text-decoration: underline;" target="_blank" href="<?php echo base_url(); ?>/eclaim.mp4">video for step by step process</a>.</p>
 			</p>
 			<p style="text-decoration: underline; font-weight: bold;">Please READ before you submit your claim online:</p>
 			<p>
 				<ul>
-					<li>You can <a style="text-decoration: underline;" href="https://eclaim.jfgroup.ca">submit your claims online</a> if your policy number begins with the following: JES, JFP, JFC, OPL, JFR, TOP.</li>
-					<li>Please <a style="text-decoration: underline;" href="https://eclaim.jfgroup.ca">submit</a> one claim for each medical condition (Example/ Doctor visit and pharmacy expense for the same condition would fall under one claim).​</li>
-					<li>Please attach pdf files or readable photos of all itemized invoices and payment receipts from the medical service provider, pharmacy or hospital along with any detailed medical reports. ​</li>
-					<li>For patients treated at a medical clinic, a physician’s medical report is required. ​</li>
-					<li>Failure to include the required supporting documentation will delay processing.​</li>
-					<li>Claim must be submitted to OTC within 90 days of occurrence.​</li>
+					<li>You can submit your claims online if your policy number begins with the following: JES, JFP, JFC, OPL, JFR, TOP.</li>
+					<li>Please submit one claim for each medical condition (Example/ Doctor visit and pharmacy expense for the same condition would fall under one claim).</li>
+					<li>Please attach pdf files or readable photos of all itemized invoices and payment receipts from the medical service provider, pharmacy or hospital along with any detailed medical reports. </li>
+					<li>For patients treated at a medical clinic, a physician’s medical report is required. </li>
+					<li>Failure to include the required supporting documentation will delay processing.</li>
+					<li>Claim must be submitted to OTC within 90 days of occurrence.</li>
 					<li>You are responsible for all fees charged for any supporting documentation with your claims.</li>
 					<li>We reserve the right to request submission of the original documentation or additional information if needed.</li>
 				</ul>
