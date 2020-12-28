81f30d3c6be1e44d4b13cbc7b6093f58d454e4e5
alvin.z <alvin.z.ca@gmail.com>
2020-12-28 17:09:21 +0000

    change email template

--- application/views/mail/package.php
+++ application/views/mail/package.php
@@ -2,24 +2,48 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <br />
-<p>Dear <?php echo htmlspecialchars($customer['firstname'] . " " . $customer['lastname']); ?>,</p>
-<p>This is a confirmation of your recent purchased insurance, please <span style="color:red;">DO NOT REPLY</span> to this email.</p>
-<br />
-<p>Thank you for purchasing medical insurance from JF Insurance Agency Group Inc.</p>
-<br />
-<p>Please refer to the attached documents for your policy confirmation, details of your coverage and claim form. Please refer to the claim form for procedures on how to claim.</p>
-<br />
-<p>If you notice any errors in your policy confirmation or need to change your trip before the effective date, please inform the agent/school/agency you purchased the insurance from immediately.</p>
-<br />
-<p>Should you have any trouble viewing the attached document, please download the latest version of Adobe Reader : http://get.adobe.com/reader.</p>
-<p></p>
-<p></p>
+Dear <?php echo htmlspecialchars($customer['firstname'] . " " . $customer['lastname']); ?>,
 <br />
 <br />
-<p>Regards,</p>
-<p>JF Insurance Agency Group Inc.</p>
-<p>15 Wertheim Court, Suite #501</p>
-<p>Richmond Hill, ON L4B 3H7</p>
-<p>Tel: 905-707-1512 Fax: 905-707-1513</p>
-<p>Email: Info@jfgroup.ca</p>
-<p>Website: www.jfgroup.ca</p>
+This is a confirmation of your recent purchased insurance,
+please
+<span style="color:red;">
+DO NOT REPLY
+</span> to this email.
+<br />
+<br />
+Thank you for purchasing medical insurance from
+JF Insurance Agency Group Inc.
+<br />
+<br />
+Please refer to the attached documents for your policy confirmation,
+details of your coverage and claim form.
+Please refer to the claim form for procedures on how to claim.
+<br />
+<br />
+If you notice any errors in your policy confirmation
+or need to change your trip before the effective date,
+please inform the agent/school/agency you purchased
+the insurance from immediately.
+<br />
+<br />
+Should you have any trouble viewing the attached document,
+please download the latest version of Adobe Reader :
+http://get.adobe.com/reader.
+<br />
+<br />
+<br />
+Regards,
+<br />
+<br />
+JF Insurance Agency Group Inc.
+<br />
+15 Wertheim Court, Suite #501
+<br />
+Richmond Hill, ON L4B 3H7
+<br />
+Tel: 905-707-1512 Fax: 905-707-1513
+<br />
+Email: Info@jfgroup.ca
+<br />
+Website: www.jfgroup.ca
