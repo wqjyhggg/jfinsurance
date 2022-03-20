4e22312ceb133d1da16eea0417214814a199a80d
alvin.z <alvin.z.ca@gmail.com>
2022-03-20 17:58:45 -0400

    for Policy version

--- application/controllers/Docs.php
+++ application/controllers/Docs.php
@@ -11,7 +11,7 @@ class Docs extends MY_Controller
   public function index()
   {
     if ($this->input->post()) {
-      $policy = preg_replace("/[^1-9A-Z]/", "", strtoupper($this->input->post('policy')));
+      $policy = preg_replace("/[^0-9A-Z]/", "", strtoupper($this->input->post('policy')));
       if ($policy) {
         $this->load->model('plan_model');
 
@@ -19,7 +19,20 @@ class Docs extends MY_Controller
           $this->data['filelist'] = array();
           if ($handle = opendir(DOWNLOADDIR)) {
             while (($file = readdir($handle)) !== false) {
-              if (preg_match("/".$plan["product_short"]."/", $file)) {
+              if (preg_match("/".$plan["product_short"]."_/", $file)) {
+                if ($file == "JFR_Policy.pdf") {
+                  if ($plan["apply_date"] <= "2018-05-31") {
+                    $file = "JFRV1_Policy.pdf";
+                  } else if ($plan["apply_date"] <= "2019-03-02") {
+                    $file = "JFRV2_Policy.pdf";
+                  } else if ($plan["apply_date"] <= "2019-07-25") {
+                    $file = "JFRV3_Policy.pdf";
+                  }
+                } else if ($file == "JES_Policy.pdf") {
+                  if ($plan["apply_date"] <= "2021-08-15") {
+                    $file = "JFRV1_Policy.pdf";
+                  }
+                }
                 $this->data['filelist'][] = $file;  // base_url("pdf/download/". $file);
               }
             }
