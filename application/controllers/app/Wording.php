7714660f26be23c4fba1877b7fad2e081e414c1b
wqjyhggg <wqjyhggg@gmail.com>
2023-09-09 11:03:57 -0400

    fix

--- application/controllers/app/Wording.php
+++ application/controllers/app/Wording.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Product extends CI_Controller
+class Wording extends CI_Controller
 {
   public $error;
   public $data;
