919979d2858320e0183a384bb7e6ca17d9924229
wqjyhggg <wqjyhggg@gmail.com>
2024-03-27 12:06:41 -0400

    Fix wording

--- application/views/mail/package_french.php
+++ application/views/mail/package_french.php
@@ -15,20 +15,27 @@ Nous vous remercions d'avoir souscrit une assurance médicale
 auprès de JF Insurance Agence Group Inc.
 <br />
 <br />
-Veuillez-vous référer aux documents ci-joints pour la confirmation
-de votre police, les détails de votre couverture et le formulaire
+Veuillez-vous référer aux documents ci-joints pour la
+confirmation de votre police, les détails de votre 
+couverture et le formulaire
 de demande de remboursement.
-Veuillez-vous référer au formulaire de demande de remboursement
+Veuillez-vous référer au formulaire de demande de 
+remboursement
 pour connaître les procédures à suivre. 
 <br />
 <br />
-Si vous constatez des erreurs dans votre confirmation de police ou
-si vous voulez modifier votre voyage avant la date d'entrée en vigueur, 
-veuillez informer immédiatement l'agent, l'école ou l'agence auprès de
-laquelle vous avez souscrit l'assurance. 
+Si vous constatez des erreurs dans votre confirmation 
+de police ou
+si vous voulez modifier votre voyage avant la date 
+d'entrée
+en vigueur, 
+veuillez informer immédiatement l'agent, 
+l'école ou l'agence
+auprès de laquelle vous avez souscrit l'assurance. 
 <br />
 <br />
-Si vous rencontrez des difficultés à visualiser le document ci-joint,
+Si vous rencontrez des difficultés à visualiser le 
+document ci-joint,
 veuillez télécharger la dernière version d'Adobe Reader
 http://get.adobe.com/reader. 
 <br />
