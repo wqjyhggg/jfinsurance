01dd109ad5fc1f276097f3cdd2612b9a01292567
wqjyhggg <wqjyhggg@gmail.com>
2024-03-26 22:18:25 -0400

    French confirmation email template

--- application/views/mail/package_french.php
+++ application/views/mail/package_french.php
@@ -6,23 +6,22 @@ Madame, Monsieur <?php echo htmlspecialchars($customer['firstname'] . " " . $cus
 <br />
 <br />
 <span style="color:red;">
-Ceci est une confirmation de l'assurance que vous avez récemment souscrite. NE RÉPONDEZ PAS à ce courriel. 
+Ceci est une confirmation de l'assurance que vous avez récemment souscrite. NE RÉPONDEZ PAS à ce courriel.
 </span>
 <br />
 <br />
 Nous vous remercions d'avoir souscrit une assurance médicale auprès de JF Insurance Agence Group Inc.
 <br />
 <br />
-Veuillez-vous référer aux documents ci-joints pour la confirmation de votre police, les détails de votre couverture et le formulaire de demande de remboursement. 
+Veuillez-vous référer aux documents ci-joints pour la confirmation de votre police, les détails de votre couverture et le formulaire de demande de remboursement.
 Veuillez-vous référer au formulaire de demande de remboursement pour connaître les procédures à suivre. 
 <br />
 <br />
-Si vous constatez des erreurs dans votre confirmation de police ou si vous devez modifier votre voyage avant la date d'entrée en vigueur, 
-veuillez en informer immédiatement l'agent, l'école ou l'agence auprès de laquelle vous avez souscrit l'assurance. 
+Si vous constatez des erreurs dans votre confirmation de police ou si vous voulez modifier votre voyage avant la date d'entrée en vigueur, 
+veuillez informer immédiatement l'agent, l'école ou l'agence auprès de laquelle vous avez souscrit l'assurance. 
 <br />
 <br />
-Si vous rencontrez des difficultés à visualiser le document ci-joint, veuillez télécharger la dernière version d'Adobe Reader : 
-http://get.adobe.com/reader.
+Si vous rencontrez des difficultés à visualiser le document ci-joint, veuillez télécharger la dernière version d'Adobe Reader http://get.adobe.com/reader. 
 <br />
 <br />
 <br />
@@ -37,6 +36,6 @@ Richmond Hill, ON L4B 3H7
 <br />
 Tél: 905-707-1512 Fax: 905-707-1513
 <br />
-Courriel : Info@jfgroup.ca
+Courriel: Info@jfgroup.ca
 <br />
 Site Internet: www.jfgroup.ca
