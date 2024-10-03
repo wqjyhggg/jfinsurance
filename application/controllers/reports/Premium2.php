5ac4a6d7a4ae8d28466074724717d96f9c0bd5af
wqjyhggg <wqjyhggg@gmail.com>
2024-10-02 21:37:09 -0400

    For local run

--- application/controllers/reports/Premium2.php
+++ application/controllers/reports/Premium2.php
@@ -195,4 +195,166 @@ class Premium2 extends MY_Controller
     $w->addRow($arr);
     $w->close();
   }
+
+  public function report()
+  {
+		if ((php_sapi_name() !== 'cli')) {
+      show_404();
+			return ;
+		}
+    $beuser = $this->user_model->get_user_by_id(1);
+    $data = array();
+    $data['beuser'] = $beuser;
+
+    $this->load->model('product_model');
+    $this->load->model('report_model');
+
+    $data['payment_added_from'] = '2022-04-01';
+    $data['payment_added_to'] = '2024-09-30';
+    $data['earned_to'] = '2024-09-30';
+    $data['product_short'] = array("JFGD","JFOS","JFPL","JFSL","JFVTC");
+
+    $data['report_data'] = $this->report_model->get_premium_report2($data);
+
+    $status_list = array(
+                1 => "Quote",
+                2 => "Sold",
+                3 => "Paid",
+                4 => "Claimed",
+                5 => "Cancel",
+                6 => "Refund",
+                7 => "Changed",
+                8 => "Adjust",
+              );
+
+    $w = WriterFactory::create(Type::XLSX); // for XLSX files
+    $kArr = array(
+      'policy' => 'Policy Number',
+      'firstname' => 'First Name',
+      'lastname' => 'Last Name',
+      'province2' => 'Province',
+      'status_id' => 'Status',
+      'sold_date' => 'Sold Date',
+      'add_time' => 'Payment Date',
+      'effective_date' => 'Effective Date',
+      'expiry_date' => 'Expire Date',
+      'totaldays' => 'Number of Days',
+      'days_used' => 'Days of Used',
+      'sum_insured' => 'Sum Insured',
+      'customer_cnt' => 'Quantity',
+      'deductible_amount' => 'Deductible Amount',
+      'dailyrate' => 'Daily Rate',
+      'discount' => 'Discounted Amount',
+      'premium' => 'Total',
+      'earned' => 'Earned',
+      'unearned' => 'Unearned',
+      'product_short' => 'Product',
+    );
+
+    $w->openToFile(DOWNLOADDIR."Premium_Report_" . date('Ymd') . ".xlsx");
+    $w->addRow(array($data['payment_added_from']. " to " . $data['payment_added_to']));
+    if (!empty($data['product_short'])) {
+      $w->addRow($data['product_short']);
+    }
+
+    $arr = array();
+    foreach ($kArr as $k => $v) {
+      $arr[] = $v;
+    }
+    $w->addRow($arr);
+
+    $total = $tearned = 0;  $solddate = '';
+    foreach ($data['report_data'] as $record) {
+      // if ($record['status_id'] == 6) { $dte = strtotime($record['expiry_date']); $dts = strtotime($record['effective_date']); $record['totaldays'] = round(($dte-$dts)/(60 * 60 * 24)) + 1; };
+      // $premium = floatval($record['dailyrate'] * $record['totaldays']);
+      // $dispremium = abs($premium) - abs($record['premium']);
+      // if ($dispremium < 0) $dispremium = 0;
+      // if (abs($premium) > abs($record['premium'])) { $premium = floatval($record['premium']); }
+      // $earned = ($record['days_used']>0)? ($premium*floatval($record['days_used'])/floatval($record['totaldays'])) : 0;
+      // $unearned = number_format($premium - $earned, 2);
+      // $total += $premium;
+      // $tearned += $earned;
+      // if ($record['ishead']==1) { $solddate = substr($record['add_time'],0,10); }
+      // $dispremium = number_format($dispremium,2);
+      // $premium = number_format($premium,2);
+      // $earned = number_format($earned,2);
+      if ($record['ishead']==1) { 
+        $solddate = substr($record['add_time'],0,10);
+      }
+      if ($record['status_id'] == 6) {
+        $dte = strtotime($record['expiry_date']);
+        $dts = strtotime($record['effective_date']);
+        $record['totaldays'] = round(($dte-$dts)/(60 * 60 * 24)) + 1; 
+      }
+      if (abs($record['premium']) <= 25) {
+        $record['premium'] = floatval($record['dailyrate'] * $record['totaldays']);
+      }
+      if ($record['days_used'] >= $record['totaldays']) {
+        $earned = $record['premium'];
+        $unearned = 0;
+      } else if ($record['days_used'] > 0) {
+        $earned = $record['premium']*$record['days_used']/$record['totaldays'];
+        $unearned = $record['premium'] - $earned;
+      } else {
+        $earned = 0;
+        $unearned = $record['premium'];
+      }
+      $total += $record['premium'];
+      $tearned += $earned;
+      $discount = 0;
+      if ($record['totaldays'] >= 365) {
+        $discount = $record['totaldays'] * $record['dailyrate'] - $record['premium'];
+        if ($discount < 5) {
+          $discount = 0;
+        }
+      }
+
+      $arr = array();
+
+      foreach ($kArr as $k => $v) {
+        if ($k == "earned") {
+          $arr[] = $earned;
+        } else if ($k == "sold_date") {
+          $arr[] = $solddate;
+        } else if ($k == "status_id") {
+          $arr[] = $status_list[$record["status_id"]];
+        } else if ($k == "add_time") {
+          $arr[] = substr($record[$k],0,10);
+        } else if ($k == "days_used") {
+          $arr[] = ($record[$k]>0)?$record[$k]:0;
+        } else if ($k == "discount") {
+          $arr[] = number_format($discount,2);
+        } else if ($k == "premium") {
+          $arr[] = number_format($record['premium'],2);
+        } else if ($k == "unearned") {
+          $arr[] = number_format($unearned,2);
+        } else if ($k == "earned") {
+          $arr[] = number_format($earned,2);
+        } else if ($k == "deductible_amount") {
+          $arr[] = number_format($record[$k], 2);
+        } else if ($k == "dailyrate") {
+          $arr[] = number_format($record[$k], 2);
+        } else {
+          $arr[] = $record[$k];
+        }
+      }
+      $w->addRow($arr);
+    }
+    $arr = array();
+    foreach ($kArr as $k => $v) {
+      if ($k == "earned") {
+        $arr[] = number_format($tearned,2);
+      } else if ($k == "unearned") {
+        $arr[] = number_format($total - $tearned,2);
+      } else if ($k == "premium") {
+        $arr[] = number_format($total,2);
+      } else if ($k == "deductible_amount") {
+        $arr[] = 'Total:';
+      } else {
+        $arr[] = '';
+      }
+    }
+    $w->addRow($arr);
+    $w->close();
+  }
 }
