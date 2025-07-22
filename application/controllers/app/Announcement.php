ce29bfa0ebd710925640fbe5dca05cdf5fa3a428
wqjyhggg <wqjyhggg@gmail.com>
2025-07-22 11:57:02 -0400

    Support upload and delete file

--- application/controllers/app/Announcement.php
+++ application/controllers/app/Announcement.php
@@ -134,4 +134,80 @@ class Announcement extends CI_Controller
     $data["user_id"] = $user_id;
     $this->app_model->return_ok($data);
   }
+
+	public function deletefile() {
+    $this->error = "";
+    $this->load->model("app_model");
+    $this->load->model("user_model");
+    $user = $this->app_model->check_token($this->input->post("token"));
+
+    if (empty($user)) {
+      if (empty($this->error)) {
+        $this->error = "Timeout";
+      }
+      return $this->app_model->return_error($this->error);
+    }
+    if ($user["user_group_id"] > 100) {
+      return $this->app_model->return_error("Unknown agent");
+    }
+    $filename = $this->input->post("filename");
+    if (file_exists(UPLOADDIR.$filename)) {
+      unlink(UPLOADDIR.$filename);
+    }
+    $this->app_model->return_ok(array());
+  }
+
+	public function upload() {
+    $this->error = "";
+    $this->load->model("app_model");
+    $this->load->model("user_model");
+    $user = $this->app_model->check_token($this->input->post("token"));
+
+    if (empty($user)) {
+      if (empty($this->error)) {
+        $this->error = "Timeout";
+      }
+      return $this->app_model->return_error($this->error);
+    }
+    if ($user["user_group_id"] > 100) {
+      return $this->app_model->return_error("Unknown agent");
+    }
+
+		if (!empty($_FILES)) {
+			$this->load->library('upload');
+			$this->load->helper('url');
+			if (!empty($_FILES['uploadfile']) && !empty($_FILES['uploadfile']['tmp_name'])) {
+				$para = array(
+						'allowed_types' => 'gif|jpg|png|pdf', 
+						'upload_path' => UPLOADDIR,
+						'file_ext_tolower' => TRUE
+				);
+				$this->upload->initialize($para);
+				if (!$this->upload->do_upload('uploadfile')) {
+          return $this->app_model->return_error($this->error);
+        } else {
+					$filedata = $this->upload->data();
+					/*
+					[file_name] => logeccbc87e4b5ce2fe28308fd9f2a7baf33.png
+					[file_type] => image/png
+					[file_path] => /home/jackw/Public/jfgroup/agentinfo/
+					[full_path] => /home/jackw/Public/jfgroup/agentinfo/logeccbc87e4b5ce2fe28308fd9f2a7baf33.png
+					[raw_name] => logeccbc87e4b5ce2fe28308fd9f2a7baf33
+					[orig_name] => logeccbc87e4b5ce2fe28308fd9f2a7baf3.png
+					[client_name] => 2.PNG
+					[file_ext] => .png
+					[file_size] => 296.71
+					[is_image] => 1
+					[image_width] => 1409
+					[image_height] => 974
+					[image_type] => png
+					[image_size_str] => width="1409" height="974"d
+					*/	
+					$data = array("imagefile" => base_url("upload/") . $filedata['file_name']);
+          $this->app_model->return_ok($data);
+				}
+			}
+		}
+    return $this->app_model->return_error("Upload failur");
+	}
 }
