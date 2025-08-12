524cfa624b6e20c281bbda80996055c2f3c06fa7
wqjyhggg <wqjyhggg@gmail.com>
2025-08-12 19:27:35 -0400

    Fix confirm TOPN

--- application/views/plan/top/pdf_note_top.php
+++ application/views/plan/top/pdf_note_top.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<P style="padding-bottom: -1em;"><B>We wish you a safe and pleasant trip!</B></P>
 			<P style="padding-bottom: -1em;"><B>Please retain this confirmation as your receipt.</B></P>
 			<P style="padding-bottom: -1em;"><B>Please read your policy before traveling as your coverage may include certain limitation and exclusion.</B></P>
+<?php if ($plan['product_short'] != 'TOPN') { ?>
 			<P style="padding-bottom: -1em;">For Trip Cancellation & Interruption Plans, coverage is not provided for any pre-existing medical condition for you, your friend, a family member, a Travelling companion or travelling companion’s family member, or a key employee, that was not stable within the 90 days (age 0-59), 180 days (age 60-74) immediately precedes the application date.</P>
+<?php } ?>
 			<P style="padding-bottom: -1em;">Coverage can be extended by contacting JF Insurance provided that a claim has not been made under this policy, that you remain eligible for insurance and that the required premium is charge to your credit card. Should you note any discrepancies in the above information, or if you have any question, please do not hesitate to contact your Travel Agent.</P>
 			<P style="padding-bottom: -1em;">Please read and understand the enclosed, which fully explains the terms, conditions, limitations and exclusions that are part of your policy.</P>
 			<P style="padding-top: 1em; padding-bottom: -1em;"><B><U>In the event of an emergency, contact Ontime Care Worldwide Inc: Toll free Canada/USA: 1-888-988-3268 or call collect 905-707-9555</U></B></P>
