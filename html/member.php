c8e3de1efd4bfa382a66241667765b49f3c844e9
wqjyhggg <wqjyhggg@gmail.com>
2025-11-12 13:21:41 -0500

    for debug

--- html/member.php
+++ html/member.php
@@ -6,18 +6,18 @@ $logFile = 'member.txt';
 function logAccess($logFile) {
     // Get the request method
     $requestMethod = $_SERVER['REQUEST_METHOD'];
-    
-    // Get GET data
     $getData = $_GET ? json_encode($_GET) : 'No GET data';
-    
-    // Get POST data
     $postData = $_POST ? json_encode($_POST) : 'No POST data';
-    
-    // Create a log entry
-    $logEntry = date('Y-m-d H:i:s') . " - Method: $requestMethod - GET: $getData - POST: $postData" . PHP_EOL;
-    
-    // Write the log entry to the file
-    file_put_contents($logFile, $logEntry, FILE_APPEND);
+		$rawInput = file_get_contents('php://input') ?: 'No raw input';
+		$headers = json_encode(getallheaders());
+		$logEntry = date('Y-m-d H:i:s') .
+				" - Method: $requestMethod" .
+				" - Headers: $headers" .
+				" - GET: $getData" .
+				" - POST: $postData" .
+				" - RAW: $rawInput" .
+				PHP_EOL;
+		file_put_contents($logFile, $logEntry, FILE_APPEND);
 }
 
 // Call the logging function
