5d205af152acf1a07e4f8e6a10874d5db6038b03
wqjyhggg <wqjyhggg@gmail.com>
2026-05-22 20:37:34 -0400

    Fix for refund

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -648,11 +648,11 @@ class Report_model extends CI_Model
 							} else if ($plan["last_status_id"] == 3) {	// Continue Plan
 								if ($plan_history["status_id"] == 3) {
 									$plan_history["premium"] = $paid_amount;
-                  $plan_history["total_premium"] = $total_premium;
+                  $plan_history["total_premium"] = $premium;
 									$report_list[] = $plan_history;
 								} else if ($plan_history["status_id"] == 8) {
 									$plan_history["premium"] = $paid_amount * -1;
-                  $plan_history["total_premium"] = $total_premium * -1;
+                  $plan_history["total_premium"] = $premium * -1;
 									$report_list[] = $plan_history;
 								}
 							} else if ($plan["last_status_id"] == 5) {	// Canceled Plan, just need monthly payment records and cancel record
