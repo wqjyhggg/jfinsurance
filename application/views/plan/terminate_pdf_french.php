3ec17093d41a1024937de44a328371ad20c236a8
wqjyhggg <wqjyhggg@gmail.com>
2026-07-11 10:26:27 -0400

    Add french terminate letter

---
+++ application/views/plan/terminate_pdf_french.php
@@ -0,0 +1,152 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PDF File</title>
+  <?php echo $style; ?>
+</head>
+
+<body>
+  <header>
+    <!--p class="rh">JF Group</p-->
+  </header>
+  <div class="container">
+    <div class="row">
+      <div style="width:390px; margin:0 auto;">
+        <div style="float:left;width:90px;">
+          <img class="img-responsive" style="width:80px;" src="<?php echo base_url(); ?>image/jf_logo.jpg" />
+        </div>
+        <div style="float:left;width:300px;text-align:center;">
+          <h3 style="margin-bottom:0;">JF Insurance Agency Group Inc.</h3>
+          <h3 style="margin-top:0;">www.jfgroup.ca</h3>
+        </div>
+      </div>
+    </div><br /><br /><br />
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        <p class="nopm"><?php echo date("F j, Y"); ?></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        <p class="nopm"><?php echo $customer_full_name; ?></p>
+        <p class="nopm">
+          <?php echo $full_address; ?><br />
+          <?php echo $city . ', ' . $province2 . ', ' . $postcode; ?></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        <p class="nopm"><b>Résiliation de la police d'assurance pour non-paiement</b></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        <p class="nopm">Cher/Chère <?php echo htmlspecialchars($customer['firstname'] . " " . $customer['lastname']); ?>,</p>
+        <p class="nopm">
+          Nous vous écrivons au sujet de votre police d'assurance <span><b><?php echo $plan['policy']; ?></b> souscrite le <b><?php echo $plan['apply_date']; ?></b></span>.
+          <?php if (!empty($monthly_data) && !empty($monthly_data["refund_record"])) { ?>
+            Nos dossiers indiquent que le paiement de cette police n'a pas été reçu le <b><?php echo $monthly_data['first_pay_fail_date']; ?></b>, et que nous
+            n'avons pas été en mesure de vous joindre pour résoudre la situation dans les 30 jours. Par conséquent,
+            votre police a été résiliée et expirera désormais le <b><?php echo $monthly_data['last_available_date']; ?></b>
+          <?php } else { ?>
+            Nous n'avons pas pu vous joindre pour régler ce problème dans les 30 jours. Par conséquent, votre police d'assurance a été résiliée et expirera le <b><?php echo $plan['expiry_date']; ?></b>.
+          <?php } ?>
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        <table class="bordered">
+          <thead>
+            <tr>
+              <th colspan="3">Nouvelle date d'expiration de la police:</th>
+            </tr>
+          </thead>
+          <?php if (empty($monthly_data) || empty($monthly_data["refund_record"])) { ?>
+            <tbody>
+              <tr>
+                <td colspan="3">Dossier de remboursement manquant</td>
+              </tr>
+            <tbody>
+            <?php } else { ?>
+            <tbody>
+              <tr>
+                <td>Prime initiale: </td>
+                <td></td>
+                <td><span>$<?php echo number_format($monthly_data['premium'], 2, '.', ','); ?></span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td><span>&nbsp;</span></td>
+              </tr>
+              <tr>
+                <td>Prime payée: </td>
+                <td><?php echo $monthly_data["refund_record"]["paid_month"]; ?> mois</td>
+                <td><span>$<?php echo number_format($monthly_data["refund_record"]["charged_amount"], 2, '.', ','); ?></span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td><span>&nbsp;</span></td>
+              </tr>
+              <tr>
+                <td>Prime impayée: </td>
+                <td><?php echo $monthly_data["refund_record"]["unpaid_month"]; ?> mois</td>
+                <td><span>$<?php echo number_format($monthly_data['premium'] - $monthly_data["refund_record"]["charged_amount"], 2, '.', ','); ?></span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td><span>&nbsp;</span></td>
+              </tr>
+              <tr>
+                <td>Date d'expiration initiale: </td>
+                <td>&nbsp;</td>
+                <td><span><?php echo $monthly_data["refund_record"]["origi_expiry_date"]; ?></span></td>
+              </tr>
+              <tr>
+                <td>Nouvelle date d'expiration: </td>
+                <td>&nbsp;</td>
+                <td><span><?php echo $monthly_data["refund_record"]["expiry_date"]; ?></span></td>
+              </tr>
+            </tbody>
+          <?php } ?>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 nopadding">
+        Si vous estimez que cette résiliation est une erreur ou si vous avez des questions, veuillez communiquer immédiatement avec votre agent afin de discuter des options possibles.<br />
+        Veuillez nous joindre au <b>(905) 707-1512</b> ou par courriel à info@jfgroup.ca.<br />
+        Nous vous remercions de l'attention immédiate que vous porterez à cette question.<br />
+      </div>
+      <div class="row">
+        <div class="col-sm-12 nopadding">
+          Cordialement,
+        </div>
+      </div>
+      <br />
+      <div class="row">
+        <div class="col-sm-12 nopadding">
+          <p><span style="border-top:1px solid #777;">Pour le compte de</span><br /> JF Insurance Agency Group Inc.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 nopadding text-center">
+          <hr class="nopm" />
+          <div class="text-center;">Siège social: 15 Wertheim Court, Suite 501, Richmond Hill, Ontario L4B 3H7</div>
+          <div class="text-center;">Téléphone: <u>905-707-1512</u> Fax:<u>905-707-1513</u> Toll free:<u>1-877-832-5541</u></div>
+        </div>
+      </div>
+    </div><!-- End Container -->
+</body>
+
+</html>
