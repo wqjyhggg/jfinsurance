0bb83ec5d3823d6e22ee4609899ea6ce2028e673
wqjyhggg <wqjyhggg@gmail.com>
2026-07-15 21:21:00 -0400

    Add French version for terminate email

--- application/views/mail/terminate_customer.php
+++ application/views/mail/terminate_customer.php
@@ -24,3 +24,24 @@ Richmond Hill, ON L4B 3H7
 Tel: 905-707-1512 Fax: 905-707-1513
 Email: Info@jfgroup.ca
 Website: www.jfgroup.ca
+
+
+
+
+Cher(e) <?php echo htmlspecialchars($customer['firstname'] . " " . $customer['lastname']); ?>,
+
+Nous vous contactons au sujet de votre police d'assurance <?php echo $plan['policy']; ?>, souscrite le <?php echo $plan['apply_date']; ?>.
+
+Nos dossiers indiquent que le paiement de cette police n'a pas été reçu et que nous n'avons pas réussi à communiquer avec vous. Par conséquent, votre police a été résiliée à compter du <?php echo $plan['expiry_date']; ?>.
+
+Si vous estimez que cette résiliation est une erreur ou si vous avez des questions, veuillez contacter immédiatement votre agent pour discuter des options possibles.
+
+Veuillez nous contacter au (905) 707-1512 ou par courriel à l'adresse info@jfgroup.ca.
+
+Nous vous remercions de l'attention immédiate que vous porterez à cette question.
+
+Cordialement,
+
+JF Insurance Agency Group Inc.
+15 Wertheim Court, Suite #501
+Richmond Hill, ON L4B 3H7
