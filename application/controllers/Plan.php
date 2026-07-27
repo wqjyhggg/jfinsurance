8507429c7e04268c5d1672f760f9a5b7b67363e1
wqjyhggg <wqjyhggg@gmail.com>
2026-07-27 00:53:40 -0400

    restore web.na,bambora.com

--- application/controllers/Plan.php
+++ application/controllers/Plan.php
@@ -3640,8 +3640,7 @@ class Plan extends MY_Controller {
     $paraString = http_build_query($params);
     $hashString = $paraString . $hashKey;
     $hash = md5($hashString);
-    // $checkoutUrl = "https://web.na.bambora.com/scripts/payment/payment.asp?" . $paraString . '&hashValue='.$hash;
-    $checkoutUrl = "https://web.bambora.com/scripts/payment/payment.asp?" . $paraString . '&hashValue='.$hash;
+    $checkoutUrl = "https://web.na.bambora.com/scripts/payment/payment.asp?" . $paraString . '&hashValue='.$hash;
     return $checkoutUrl;
   }
 
