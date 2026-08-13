0902098d002c90e4a09612e535285dc3d650da0d
wqjyhggg <wqjyhggg@gmail.com>
2026-08-13 11:58:48 -0400

    Insurer admin fee bug fix

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -297,7 +297,7 @@ class Report_model extends CI_Model
     $sql .= "	pl.trip_cancellation_ck,";
     $sql .= "	pl.trip_cancellation_insured,";
     $sql .= "	pl.deductible_amount,";
-    $sql .= "	(pa.amount - pa.admin_fee) AS policy_premium,";
+    $sql .= "	(pa.amount + pa.admin_fee) AS policy_premium,";
     $sql .= "	(100 - pr.up_pay_rate) AS commission_rate_jf,";
     $sql .= "	pa2.amount AS pr_commission,";
     $sql .= "	pa3.amount AS up_commission,";
